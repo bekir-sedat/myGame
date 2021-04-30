d46b24896a3d01818db840d9d822c5d38fc2a175
bekir-sedat <53154871+bekir-sedat@users.noreply.github.com>
2021-04-30 17:13:01 -0500

    Update Block.cs

--- Block.cs
+++ Block.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public abstract class Block : MonoBehaviour
 {
-
-
-
     public GameObject[] bricks;
     public bool isMoving;
-
-
-
     public abstract void RemoveAvailableNodes();
-
     public abstract void Rotate90();
-
-
 }
