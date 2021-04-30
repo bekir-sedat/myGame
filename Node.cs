244e082cefd1f48d2cb7b7c0dbaeacbd1d84fd80
bekir-sedat <53154871+bekir-sedat@users.noreply.github.com>
2021-04-30 17:12:33 -0500

    Update Node.cs

--- Node.cs
+++ Node.cs
@@ -4,32 +4,23 @@ using UnityEngine;
 
 public class Node
 {
-
     public int x;
     public int y;
-
     public Vector3 worldPos;
-
     public GameObject brick;
-
     public Node(int x, int y)
     {
-
         this.x = x;
         this.y = y;
-
         worldPos = new Vector3(x, y, 0);
-
     }
-
     public override int GetHashCode()
     {
         return x * 11 + y * 211;
     }
-
+    
     public override bool Equals(object obj)
     {
         return x == ((Node)obj).x && y == ((Node)obj).y;
     }
-
 }
